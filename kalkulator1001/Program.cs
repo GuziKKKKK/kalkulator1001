1fc38fa33741c1794265c70bd6608087bd92df1c
GuziKKKKK <guzik.owy@o2.pl>
2019-09-22 15:30:39 +0200

    Introduce menu to application

--- kalkulator1001/Program.cs
+++ kalkulator1001/Program.cs
@@ -10,6 +10,17 @@ namespace kalkulator1001
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                int menuOption = Convert.ToInt16(Console.ReadLine());
+                if (menuOption == 0)
+                {
+                    break;
+                }
+            }
         }
     }
 }
