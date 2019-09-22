c9638bfec5630afa2877fd4fb27d8c6d83109224
GuziKKKKK <guzik.owy@o2.pl>
2019-09-22 15:39:15 +0200

    Add multiplation to menu

--- kalkulator1001/Program.cs
+++ kalkulator1001/Program.cs
@@ -15,6 +15,8 @@ namespace kalkulator1001
                 Console.WriteLine("0. Exit");
                 Console.WriteLine("1. Dodawanie");
                 Console.WriteLine("2. Odejmowanie");
+                Console.WriteLine("3. Mnożenie");
+
                 int menuOption = Convert.ToInt16(Console.ReadLine());
                 if (menuOption == 0)
                 {
